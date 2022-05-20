e73a24dcdc0bba4fae0b560696e1322c52840115
Kenneth Adjei <[kmadjei25@gmail.com]>
2022-05-20 11:24:25 -0400

    Setup dotenv file for AWS url, user, and password

--- src/dao/Database.java
+++ src/dao/Database.java
@@ -4,17 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Database {
-	private static final String url = 
-			"jdbc:postgresql://localhost/bankApp"
+	
+	static Dotenv dotenv = Dotenv.load();
+	
+	private static final String url =
+			"jdbc:postgresql://" + dotenv.get("AWS_RDS_URL")
+			//"jdbc:postgresql://localhost/bankApp"
 			//jdbc:postgresql://revature-p0.cyexqwl9xulr.ca-central-1.rds.amazonaws.com";
 			;
-    private static final String user = "postgres";
-    private static final String password = "password";
+    private static final String user = dotenv.get("AWS_DB_USERNAME");
+    private static final String password = dotenv.get("AWS_DB_PASSWORD");;
     
     private static Database instance = new Database(); //load single instance
     
-    // private construct allows for Database to have one instance
+    // private constructor allows for Database to have one instance
     private Database() {} 
     
     public static Database getInstance() {
